f22a2c9d01a3dec43d7a7d7f70e84fb85c703348
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-08-03 22:34:24 +0530

    add custom logger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import logger from "./utils/logger";
 
 const app = express();
 const PORT = process.env.PORT || "8090";
@@ -13,5 +14,5 @@ app.get("/", (req, res, next) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is up and running on PORT ${PORT}`)
+    logger.info(`🚀 Server is up and running on PORT ${PORT}`)
 })
