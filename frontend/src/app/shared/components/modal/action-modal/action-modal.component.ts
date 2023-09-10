4211ef7a8ad373ed03ef2ce372fe9a59003bcb5f
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-09-11 00:00:00 +0530

    add action modal

---
+++ frontend/src/app/shared/components/modal/action-modal/action-modal.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LucideAngularModule, ShieldAlert, ShieldCheck, ShieldQuestion, ShieldX } from 'lucide-angular';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-action-modal',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule],
+  templateUrl: './action-modal.component.html',
+  styleUrl: './action-modal.component.scss',
+})
+export class ActionModalComponent {
+  @Input() isOpen: boolean = false;
+  @Input() type: 'info' | 'success' | 'error' | 'warning' = 'info';
+  @Input() title: string = 'Confirm';
+  @Input() message: string = 'Are you sure you want to proceed?';
+
+  @Output() onConfirm = new EventEmitter<boolean>();
+
+  readonly ShieldQuestion = ShieldQuestion;
+  readonly Success = ShieldCheck;
+  readonly Error = ShieldX;
+  readonly Warning = ShieldAlert;
+
+  handleYes(): void {
+    this.onConfirm.emit(true);
+    this.isOpen = false;
+  }
+
+  handleNo(): void {
+    this.onConfirm.emit(false);
+    this.isOpen = false;
+  }
+}
