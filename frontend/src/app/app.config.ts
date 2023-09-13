fa1d86a8d05e4df62b69093f9d984af5995c53a6
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-09-14 00:00:00 +0530

    Update Angular configuration: add allowedCommonJsDependencies and adjust budget limits

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,33 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideState, provideStore } from '@ngrx/store';
 
 import { ApplicationConfig } from '@angular/core';
+import { AuthEffects } from './store/effects/auth.effects';
+import { BooksEffects } from './store/effects/book.effects';
+import { MemberEffects } from './store/effects/member.effect';
+import { UserEffects } from './store/effects/user.effects';
 import { authInterceptor } from './core/interceptor/auth.interceptor';
+import { authReducer } from './store/reducers/auth.reducer';
+import { booksReducer } from './store/reducers/books.reducer';
+import { memberReducer } from './store/reducers/member.reducer';
+import { provideEffects } from '@ngrx/effects';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
+import { userReducer } from './store/reducers/user.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideStore()
-]
+    provideStore(),
+    provideEffects(),
+    provideState('books', booksReducer),
+    provideState('members', memberReducer),
+    provideState('users', userReducer),
+    provideState('auth', authReducer),
+    provideEffects(UserEffects),
+    provideEffects(MemberEffects),
+    provideEffects(BooksEffects),
+    provideEffects(AuthEffects)
+  ]
 };
