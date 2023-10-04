7591c8033bf622f320cb889060879815470c7fbd
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-10-05 00:00:00 +0530

    Add a standalone date format pipe for consistent date formatting

---
+++ frontend/src/app/shared/pipes/date-format.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat',
+  standalone: true
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: string | Date, format: string = 'shortDate'): string {
+
+    const date = new Date(value);
+    if (isNaN(date.getTime())) {
+      return '';
+    }
+
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    });
+  }
+}
