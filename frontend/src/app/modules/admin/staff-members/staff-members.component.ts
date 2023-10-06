12e7a400f4570b7ede9a90f3e89d450db38ace8c
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-10-07 00:00:00 +0530

    Refactor staff members component: update user list display, implement pagination, and integrate NgRx for state management

--- frontend/src/app/modules/admin/staff-members/staff-members.component.ts
+++ frontend/src/app/modules/admin/staff-members/staff-members.component.ts
@@ -1,3 +1,5 @@
+import * as UserActions from '../../../store/actions/user.actions';
+
 import {
   FilePenLine,
   LucideAngularModule,
@@ -5,36 +7,57 @@ import {
   SquarePlus,
   Trash2,
 } from 'lucide-angular';
+import { selectUserPagination, selectUsers } from '../../../store/selectors/user.selector';
 
 import { ApiService } from '../../../core/services/api.service';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DateFormatPipe } from '../../../shared/pipes/date-format.pipe';
 import { FormsModule } from '@angular/forms';
 import { IUser } from '../../../interfaces/user';
+import { Observable } from 'rxjs';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-staff-members',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule, FormsModule],
+  imports: [CommonModule, LucideAngularModule, FormsModule, PaginationComponent, DateFormatPipe],
   templateUrl: './staff-members.component.html',
   styleUrl: './staff-members.component.scss',
 })
 export class StaffMembersComponent {
-  users: IUser[] = [];
+  users$: Observable<IUser[]> | undefined;
+  currentPage = 1;
+  pageSize = 10;
+  totalPages = 0;
+  totalItems = 0;
   filteredUsers: IUser[] = [];
   searchTerm: string = '';
   errorMessage: string = '';
   isModalOpen: boolean = false;
+  pagination$: Observable<any> | undefined;
 
   readonly Search = Search;
   readonly SquarePlus = SquarePlus;
   readonly FilePenLine = FilePenLine;
   readonly Trash2 = Trash2;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private store: Store) {
+    this.users$ = this.store.select(selectUsers);
+    this.pagination$ = this.store.select(selectUserPagination);
+  }
 
   ngOnInit(): void {
     this.loadUsers();
+    this.pagination$?.subscribe((pagination) => {
+      if (pagination) {
+        this.currentPage = pagination.currentPage;
+        this.totalItems = pagination.totalItems;
+        this.totalPages = pagination.totalPages;
+        this.pageSize = pagination.pageSize;
+      }
+    });
   }
 
   addUser() {}
@@ -48,34 +71,38 @@ export class StaffMembersComponent {
   }
 
   loadUsers(): void {
-    this.apiService.getUsers().subscribe({
-      next: (data) => {
-        if (data.success && Array.isArray(data.data)) {
-          this.users = data.data;
-          this.filteredUsers = [...this.users];
-          console.log('Users:', data.data);
-        } else {
-          this.errorMessage = 'No users found.';
-        }
-      },
-      error: (err) => {
-        console.error('Error fetching users:', err);
-        this.errorMessage = 'Error fetching users.';
-      },
-    });
+    this.store.dispatch(UserActions.loadUsers({ page: this.currentPage, limit: this.pageSize }));
   }
 
   filterUsers(): void {
-    if (this.searchTerm.trim() === '') {
-      this.filteredUsers = [...this.users];
-    } else {
-      this.filteredUsers = this.users.filter((user) =>
-        user.username.toLowerCase().includes(this.searchTerm.toLowerCase())
-      );
-    }
+    this.store.dispatch(UserActions.filterUsers({ searchTerm: this.searchTerm, pageSize: this.pageSize, page: this.currentPage }));
   }
 
   formatDate(date: string): string {
     return new Date(date).toLocaleDateString();
+  }
+
+  updatePagination() {
+    this.loadUsers();
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
+
+  onLimitChange(limit: number) {
+    this.pageSize = limit;
+    this.currentPage = 1;
+    this.updatePagination();
   }
 }
