222eaeeaa0ee43a00e8d683a7d9c39983377eec8
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-10-11 00:00:00 +0530

    Refactor user list view component: implement async data handling, enhance layout, and add user modal

--- frontend/src/app/shared/components/user-list-view/user-list-view.component.ts
+++ frontend/src/app/shared/components/user-list-view/user-list-view.component.ts
@@ -1,61 +1,76 @@
+import * as UserActions from '../../../store/actions/user.actions';
+
 import {
   FilePenLine,
   LucideAngularModule,
-  Trash,
-  Trash2,
+  Trash2
 } from 'lucide-angular';
+import { selectUserError, selectUserLoading, selectUsers } from '../../../store/selectors/user.selector';
 
-import { ApiService } from '../../../core/services/api.service';
+import { AddUserModalComponent } from "../modal/add-user-modal/add-user-modal.component";
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IUser } from '../../../interfaces/user';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-user-list-view',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule],
+  imports: [CommonModule, LucideAngularModule, AddUserModalComponent],
   templateUrl: './user-list-view.component.html',
   styleUrl: './user-list-view.component.scss',
 })
 export class UserListViewComponent {
-  users: IUser[] = [];
   readonly FilePenLine = FilePenLine;
   readonly Trash2 = Trash2;
   errorMessage: string = '';
+  userRole: string = '';
+  isAddUserModal: boolean = false
+  isModalOpen: boolean = false;
+  isErrorModal: boolean = false;
+  isErrorModalOpen: boolean = false;
+  users$: Observable<IUser[]> | undefined;
+  pagination$: Observable<any> | undefined;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private store: Store) {
+    this.users$ = this.store.select(selectUsers);
+    this.store.select(selectUserLoading).subscribe((loading) => {
+      if (!loading) {
+        this.store.select(selectUserError).subscribe((error) => {
+          if (error) {
+            this.isErrorModalOpen = true;
+            this.errorMessage = error;
+          }
+        });
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.loadUsers();
   }
 
   addUser() {
-    alert('Add New User functionality to be implemented!');
-  }
-
-  editUser(user: any) {
-    alert(`Edit User: ${user.name}`);
-  }
-
-  deleteUser(userId: string) {
-    // this.users = this.users.filter(user => user.id !== userId);
-    alert(`User with ID ${userId} deleted.`);
+    this.isAddUserModal = true;
   }
 
   loadUsers(): void {
-    this.apiService.getUsers().subscribe({
-      next: (data) => {
-        if (data.success) {
-          this.users = data.data;
-          console.log('Books:', data);
-        } else {
-          this.errorMessage = 'No books found or invalid data format.';
-        }
-      },
-      error: (err) => {
-        console.error('Error fetching books:', err);
-        this.errorMessage = 'Error fetching books.';
-      },
-    });
+    this.store.dispatch(UserActions.loadUsers({ page: 1, limit: 7 }));
+  }
+
+  registerUser() {
+    this.isAddUserModal = false;
+    this.loadUsers();
+  }
+
+  handleError(message: any) {
+    this.isErrorModal = true;
+    this.errorMessage = message;
+    this.isModalOpen = false;
+  }
+
+  closeUserModal() {
+    this.isModalOpen = false;
   }
 }
