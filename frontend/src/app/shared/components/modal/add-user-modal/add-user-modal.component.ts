da383198666effef421a9a42c5f47e1cef3f06a8
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2023-10-12 00:00:00 +0530

    Refactor add book modal component: improve layout, enhance responsiveness, and integrate error handling

--- frontend/src/app/shared/components/modal/add-user-modal/add-user-modal.component.ts
+++ frontend/src/app/shared/components/modal/add-user-modal/add-user-modal.component.ts
@@ -1,54 +1,76 @@
+import * as UserActions from '../../../../store/actions/user.actions';
+
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { LucideAngularModule, Users } from 'lucide-angular';
+import { selectUserError, selectUserLoading } from '../../../../store/selectors/user.selector';
 
 import { AlertModalComponent } from '../alert-modal/alert-modal.component';
 import { ApiService } from '../../../../core/services/api.service';
 import { BarcodeComponent } from '../../barcode/barcode.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-add-user-modal',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule, FormsModule, AlertModalComponent],
+  imports: [CommonModule, LucideAngularModule, FormsModule],
   templateUrl: './add-user-modal.component.html',
   styleUrl: './add-user-modal.component.scss'
 })
 export class AddUserModalComponent {
   isAlertOpen: boolean = true;
-  @Input() isOpen: boolean = false; // Controls modal visibility
+  @Input() isOpen: boolean = false;
   @Input() title: string = 'Confirm';
   @Input() message: string = 'Are you sure you want to proceed?';
 
   @Output() onConfirm = new EventEmitter<boolean>();
   @Output() closeModal = new EventEmitter<void>();
-  @Output() addMember = new EventEmitter<any>();
+  @Output() addUser = new EventEmitter<void>();
+  @Output() errorOccurred = new EventEmitter<string>();
 
   newMember = {
     name: '',
     email: '',
-    membershipType: '',
+    password: '',
+    username: '',
+    role: 'Staff'
   };
 
   readonly Users = Users;
 
-  constructor(private apiService: ApiService) {}
-
-  handleRegister() {
-    this.apiService.addBook(this.newMember).subscribe({
-      next: (data) => {
-        if (data.success) {
-          this.addMember.emit(data.data);
-        } else {
-          console.error('Error adding book:', data.error);
-        }
-      },
-      error: (err) => {
-        console.error('Error adding book:', err);
+  constructor(private store: Store) {
+    this.store.select(selectUserLoading).subscribe((loading) => {
+      if (!loading) {
+        this.store.select(selectUserError).subscribe((error) => {
+          if (error) {
+            this.errorOccurred.emit(error);
+            this.isOpen = false;
+            this.resetForm();
+          } else {
+            this.addUser.emit();
+            this.onConfirm.emit(true);
+            this.isOpen = false;
+            this.resetForm();
+          }
+        });
       }
     });
-    this.onConfirm.emit(true);
-    this.isOpen = false;
+  }
+
+  handleRegister(form: NgForm) {
+    if (form.valid) {
+      this.store.dispatch(UserActions.addUser({ data: this.newMember }));
+
+      this.isOpen = false;
+      this.resetForm();
+    } else {
+      form.controls['name'].markAsTouched();
+      form.controls['email'].markAsTouched();
+      form.controls['password'].markAsTouched();
+      form.controls['username'].markAsTouched();
+      form.controls['role'].markAsTouched();
+    }
   }
 
   handleCancel() {
@@ -61,7 +83,9 @@ export class AddUserModalComponent {
     this.newMember = {
       name: '',
       email: '',
-      membershipType: '',
+      username: '',
+      role: 'Staff',
+      password: '',
     };
   }
 }
